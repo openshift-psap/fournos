1e60eba123341634ead7aa953746f3655ca951a9
avasilev <avasilev@redhat.com>
2026-04-21 13:04:44 +0200

    Improve status message and logging for jobs waiting on exclusively locked clusters
    Signed-off-by: avasilev <avasilev@redhat.com>

--- fournos/core/constants.py
+++ fournos/core/constants.py
@@ -2,6 +2,7 @@ from enum import StrEnum
 
 LABEL_MANAGED_BY = "app.kubernetes.io/managed-by"
 LABEL_JOB_NAME = "fournos.dev/job-name"
+LABEL_EXCLUSIVE_CLUSTER = "fournos.dev/exclusive-cluster"
 
 CLUSTER_SLOT_RESOURCE = "fournos/cluster-slot"
 MAX_CLUSTER_SLOTS = 100
